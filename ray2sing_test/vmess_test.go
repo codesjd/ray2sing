71f2158f81d9ac27ed47f81959352b9dc1f38d29
Ali <thealisajedi@gmail.com>
2026-07-30 14:01:12 +0330

    032: add TestVmess_TlsWebsocket protocol test coverage

--- ray2sing_test/vmess_test.go
+++ ray2sing_test/vmess_test.go
@@ -28,3 +28,42 @@ func TestVmess(t *testing.T) {
 	`
 	ray2sing.CheckUrlAndJson(url, expectedJSON, t)
 }
+
+func TestVmess_TlsWebsocket(t *testing.T) {
+	url := "vmess://eyJhZGQiOiI1MS4xNjEuMTMwLjE3MyIsImFpZCI6IjAiLCJhbHBuIjoiIiwiZnAiOiJjaHJvbWUiLCJob3N0Ijoidm1lc3MuZXhhbXBsZS5jb20iLCJpZCI6ImQ0M2VlNWUzLTFiMDctNTZkNy1iMmVhLThkMjJjNDRmZGM2NiIsIm5ldCI6IndzIiwicGF0aCI6Ii92bWVzc3BhdGgiLCJwb3J0IjoiNDQzIiwic2N5IjoiYXV0byIsInNuaSI6InZtZXNzLmV4YW1wbGUuY29tIiwidGxzIjoidGxzIiwidHlwZSI6Im5vbmUiLCJ2IjoiMiIsInBzIjoidm1lc3MtdGxzLXdzLXRlc3QifQo="
+
+	expectedJSON := `
+	{
+		"outbounds": [
+		  {
+			"type": "vmess",
+			"tag": "vmess-tls-ws-test § 0",
+			"server": "51.161.130.173",
+			"server_port": 443,
+			"uuid": "d43ee5e3-1b07-56d7-b2ea-8d22c44fdc66",
+			"security": "auto",
+			"authenticated_length": true,
+			"packet_encoding": "xudp",
+			"tls": {
+			  "enabled": true,
+			  "server_name": "vmess.example.com",
+			  "alpn": "http/1.1",
+			  "utls": {
+				"enabled": true,
+				"fingerprint": "chrome"
+			  }
+			},
+			"transport": {
+			  "type": "ws",
+			  "path": "/vmesspath",
+			  "headers": {
+				"Host": "vmess.example.com"
+			  },
+			  "early_data_header_name": "Sec-WebSocket-Protocol"
+			}
+		  }
+		]
+	  }
+	`
+	ray2sing.CheckUrlAndJson(url, expectedJSON, t)
+}
